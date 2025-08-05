f70897922fd9f56306639fe277441b9cdb88905d
André Lucas <adg@adgdev.me>
2025-08-05 18:36:42 -0300

    Update route.ts

--- src/app/api/experience/route.ts
+++ src/app/api/experience/route.ts
@@ -8,11 +8,11 @@ export function GET() {
         "Fui responsável pelo desenvolvimento do Ginga, um app para Discord que gerencia bolões de esportes e eSports. Atuei tanto na construção das Discord Activities, quanto no bot do Ginga, além de realizar manutenções e participar de eventos internos na equipe de desenvolvimento."
     },
     {
-      title: "Ex-Cofundador e Desenvolvedor",
+      title: "Cofundador e Desenvolvedor",
       company: "DreamTeam",
       period: "Outubro 2022 - Julho 2025",
       description:
-        "Ex-cofundador e principal desenvolvedor do DreamTeam, um bot de simulação de futebol para Discord, presente em mais de 65 mil servidores e com 180 mil usuários registrados. Liderava o desenvolvimento técnico utilizando TypeScript, Node.js, PostgreSQL, Redis, Docker e Prisma ORM, garantindo escalabilidade, segurança e performance. Desenvolvi sistemas completos para partidas, pacotes de jogadores, clubes personalizáveis, economia virtual e rankings globais, além de implementar painéis interativos com Discord Components v2, interações avançadas com cache inteligente e renderização dinâmica de imagens e GIFs. Também gerenciava a infraestrutura com Docker e deploy contínuo, além de interagir ativamente com a comunidade para evoluir o produto com base em feedbacks."
+        "Cofundador e principal desenvolvedor do DreamTeam, um bot de simulação de futebol para Discord, presente em mais de 65 mil servidores e com 180 mil usuários registrados. Liderava o desenvolvimento técnico utilizando TypeScript, Node.js, PostgreSQL, Redis, Docker e Prisma ORM, garantindo escalabilidade, segurança e performance. Desenvolvi sistemas completos para partidas, pacotes de jogadores, clubes personalizáveis, economia virtual e rankings globais, além de implementar painéis interativos com Discord Components v2, interações avançadas com cache inteligente e renderização dinâmica de imagens e GIFs. Também gerenciava a infraestrutura com Docker e deploy contínuo, além de interagir ativamente com a comunidade para evoluir o produto com base em feedbacks."
     }
   ];
 
